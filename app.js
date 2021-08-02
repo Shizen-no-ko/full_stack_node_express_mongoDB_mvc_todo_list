af8fbadd2b458ba50964f26ae22b75d1db513897
Mike Search <search_michael@yahoo.com>
2021-08-02 17:17:06 +0200

    Adds rough css

--- app.js
+++ app.js
@@ -2,4 +2,13 @@ const express = require('express');
 
 const app = express();
 
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+app.use(express.static(__dirname + '/public'));
+
+app.get("/", (req, res, next) => {
+    res.render("index", {pageTitle: "Home" });
+});
+
 app.listen(3000);
